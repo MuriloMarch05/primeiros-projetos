48af33ee04301415c3554a670861c13428a8d832
MuriloMarch05 <murilomarchiori17@gmail.com>
2025-10-18 14:47:58 -0300

    Adiciona cálculo da soma de valores pares, soma da terceira coluna e maior valor da segunda linha

--- Python/Estudos/Mundo_3/Exercícios/ex087.py
+++ Python/Estudos/Mundo_3/Exercícios/ex087.py
@@ -1,4 +1,7 @@
 matriz = [[0,0,0], [0,0,0], [0,0,0]]
+soma_pares = 0
+maior_segunda_linha = 0
+soma_terceira_coluna = 0
 
 for l in range(0,3):
     for c in range(0,3):
@@ -8,8 +11,20 @@ print('-=' * 30)
 for l in range(0,3):
     for c in range(0,3):
         print(f'[{matriz[l][c]:^5}]', end='')
+        if matriz[l][c] % 2 == 0:
+            soma_pares += matriz[l][c]
     print()
+print('-=' * 30)
+print(f'A soma dos valores pares é {soma_pares}')
 
+for l in range(0,3):
+    soma_terceira_coluna += matriz[l][2]
+print(f'A soma dos valores da terceira coluna é {soma_terceira_coluna}')
 
+for c in range(0,3):
+    if c == 0:
+        maior_segunda_linha = matriz[1][c]
+    elif matriz[1][c] > maior_segunda_linha:
+        maior_segunda_linha = matriz[1][c]
 
-
+print(f'O maior valor da segunda linha é {maior_segunda_linha}')
