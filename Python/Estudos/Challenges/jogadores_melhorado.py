89bb5b8bf671ac84aaf39138e3f49fc71add2e32
MuriloMarch05 <murilomarchiori17@gmail.com>
2025-11-22 09:41:45 -0300

    Adiciona script para coleta de dados de desempenho de jogadores de futebol

---
+++ Python/Estudos/Challenges/jogadores_melhorado.py
@@ -0,0 +1,35 @@
+dados = dict()  # Dicionário para armazenar os dados do jogador
+gols = list()   # Lista para armazenar os gols por partida
+
+dados['nome'] = str(input('Nome do jogador: ')).title()
+partidas = int(input(f'Quantas partidas {dados["nome"]} jogou? '))
+
+for c in range(0, partidas):    # Loop para coletar os gols em cada partida
+    gols.append(int(input(f'  Quantos gols na partida {c + 1}?')))
+
+dados['gols'] = gols[:] # Armazena uma cópia da lista gols em dados['gols']
+dados['total'] = sum(gols) # Calcula o total de gols e armazena em dados['total']
+media = dados['total'] / partidas if partidas > 0 else 0 # Calcula a média de gols por partida
+
+print('-=' * 30)
+print(dados)
+print('-=' * 30)
+
+for k, v in dados.items():  # Itera sobre os itens do dicionário dados
+    print(f'O campo {k} tem valor {v}.')
+
+print('-=' * 30)
+
+print(f'O jogador {dados["nome"]} jogou {partidas} partidas.')
+for i, v in enumerate(dados['gols']):   # Itera sobre a lista de gols com índice
+    print(f'  => Na partida {i + 1}, {dados["nome"]} fez {v} gols.')
+
+melhor_partida = dados['gols'].index(max(dados['gols'])) + 1    # Índice da melhor partida
+pior_partida = dados['gols'].index(min(dados['gols'])) + 1  # Índice da pior partida
+
+print('-=' * 30)
+print('RESUMO DO JOGADOR'.center(60))
+print('-=' * 30)
+print(f'NOME: {dados["nome"]}\nPARTIDAS: {partidas}\nTOTAL DE GOLS: {dados["total"]}\nMÉDIA DE GOLS POR PARTIDA: {media:.1f}')
+print(f'MELHOR PARTIDA: Partida {melhor_partida} com {max(dados["gols"])} gols')
+print(f'PIOR PARTIDA: Partida {pior_partida} com {min(dados["gols"])} gols.')
