0dea6b0e846f362c9ea5e208aaf2c6fe8571f5ea
MuriloMarch05 <murilomarchiori17@gmail.com>
2025-12-02 17:22:30 -0300

    Adiciona script para cálculo da área de terrenos

---
+++ Python/Estudos/Mundo_3/Exercícios/ex096.py
@@ -0,0 +1,17 @@
+def calcular_area(l, c):
+    area = l*c
+    print(f'A área de um terreno {l}m x {c}m é de {area:.2f}m².')
+
+
+def linha():
+    print('-' * 30)
+
+linha()
+print('CONTROLE DE TERRENOS'.center(30))
+linha()
+
+largura = float(input('Largura (m): '))
+comprimento = float(input('Comprimento (m): '))
+
+calcular_area(largura, comprimento)
+linha()
