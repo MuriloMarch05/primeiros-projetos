9423ee1145e129178276c7ac4ab232060f4f99df
MuriloMarch05 <murilomarchiori17@gmail.com>
2025-12-09 10:42:54 -0300

    Adiciona funções para sortear números aleatórios e somar valores pares

---
+++ Python/Estudos/Mundo_3/Exercícios/ex100.py
@@ -0,0 +1,25 @@
+from random import randint
+from time import sleep
+
+
+def sorteia():
+    print('Sorteando 5 valores da lista: ', end='')
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        numeros.append(n)
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+    print('PRONTO!')
+
+
+def somaPar():
+    soma = 0
+    for n in numeros:
+        if n % 2 == 0:
+            soma += n
+    print(f'Somando os valores pares entre {numeros}, temos {soma}')
+
+
+numeros = list()
+sorteia()
+somaPar()
