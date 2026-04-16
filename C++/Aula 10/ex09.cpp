c45c7c03f3904d5fba6c2eb3500a671b5d29c4ec
Murilo Marchiori <murilomarchiori17@gmail.com>
2026-04-16 17:40:36 -0300

    feat: implement electronic voting system with two candidates

---
+++ C++/Aula 10/ex09.cpp
@@ -0,0 +1,66 @@
+/*9) Uma eleição possui 2 candidatos a prefeito: o candidato José, do Partido da Felicidade e o candidato
+João, do Partido da Esperança. Os eleitores votam nos candidatos pelo número do partido. Para votar no
+candidato José do Partido da Felicidade, precisam digitar o número 1. Para votar no candidato João, do
+Partido da Esperança, precisam digitar o número 2. Faça um programa que simule uma urna eletrônica.
+Para cada eleitor, deve ser lido o número do seu título e em seguida o eleitor poderá digitar o seu voto. A
+eleição é encerrada quando um valor negativo (menor do que 0) para o título do eleitor é digitado. Informe
+quantos votos cada candidato teve e qual deles ganhou a eleição.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    int titulo=0, escolha=0, votos1=0, votos2=0;
+
+    do
+    {
+        cout << "Título (número negativo para parar): ";
+        cin >> titulo;
+
+        if (titulo < 0)
+            {
+                break;
+            }
+
+        cout << "Vote\n1-José\n2-João" << endl;
+        cin >> escolha;
+
+        if(escolha == 1)
+            {
+                cout << "Você votou em José do partido Felicidade.\n";
+                votos1 += 1;
+            }
+        else if (escolha == 2)
+            {
+                cout << "Você votou em João do partido Esperança.\n";
+                votos2 += 1;
+            }
+        else
+            {
+                cout << "Opção inválida.";
+            }
+
+
+    }while(titulo > 0);
+
+
+    cout << "O candidato José teve " << votos1 << " voto(s)" << endl;
+    cout << "O candidato João teve " << votos2 << " voto(s)" << endl;
+
+        if (votos1 > votos2)
+            {
+            cout << "O vencedor foi José!";
+            }
+
+        else if (votos1 < votos2)
+            {
+                cout << "O vencedor foi João!";
+            }
+        else
+            {
+                cout << "Empate técnico! Necessário segundo turno.";
+            }
+}
