12b4412487cc1164f58ed2bfa32c626d80cf98d4
Murilo Marchiori <murilomarchiori17@gmail.com>
2026-04-28 20:13:54 -0300

    feat: implement program to store and print integers from 1 to 15

---
+++ C++/Aula 11/vetor_15.cpp
@@ -0,0 +1,18 @@
+/*
+1) Escreva um programa que armazene em um vetor todos os números inteiros de 1 a 15. Após
+isso, o programa deve imprimir todos os valores armazenados.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int vet[15], i;
+	
+	for(i=1;i<=15;i++)
+	{
+		cout << vet[15] << " ";
+	}
+	
+	return 0;	
+}
