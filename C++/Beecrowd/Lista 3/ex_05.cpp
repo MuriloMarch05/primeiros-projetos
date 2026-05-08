6ecabe7022ada32d35fc0b014774c3d7785c0fc9
MuriloMarch05 <murilomarchiori17@gmail.com>
2026-05-08 11:43:12 -0300

    Add program to find the smallest element in an array and its position
    - Implemented a C++ program that reads an integer N to define the size of an array X[N].
    - The program then reads N values into the array and identifies the smallest value along with its index.
    - Included user prompts for input and output of results.

---
+++ C++/Beecrowd/Lista 3/ex_05.cpp
@@ -0,0 +1,37 @@
+/*Faça um programa que leia um valor N. Este N será o tamanho de um vetor X[N]. 
+A seguir, leia cada um dos valores de X, encontre o menor elemento deste vetor e a sua posição dentro do vetor, 
+mostrando esta informação.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, i, menor, posicao;
+
+    cout << "Digite o tamanho do vetor: ";
+    cin >> N;
+    int X[N]; // Declaração do vetor com tamanho N. "Por que não declarou antes?" 
+            //Pois não seria possível determinar o tamanho do vetor sem antes ler o valor de N.
+
+    for(i=0;i<N;i++)
+    {
+        cout << "Digite o valor do vetor: ";
+        cin >> X[i];
+
+        if(i == 0)
+        {
+            menor = X[i];
+        }
+        else if (X[i] < menor)
+        {
+            menor = X[i];
+            posicao = i; // Armazena o índice do menor valor.
+        }
+    }
+
+    cout << "Menor valor: " << menor << endl;
+    cout << "Posição do menor valor: " << posicao << endl;
+
+    return 0;
+}
